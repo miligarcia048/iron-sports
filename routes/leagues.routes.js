b65274e20fad8eb80282b9299e74c8a9fe81b735
Milagros Garcia <miligarcia048@gmail.com>
2022-12-03 14:50:05 +0000

    leagues api

--- routes/leagues.routes.js
+++ routes/leagues.routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+const ApiService = require('../services/leagues.service');
+const apiService = new ApiService();
+
 router.get("/leagues", async (req, res, next) => {
     try {
-			 const allLeagues = await apiService.getAllLEagues();
-        res.render("leagues/leagues" , {leagues: allLeagues });
-
-
+        const allLeagues = await apiService.getAllLeagues();
+        res.render("leagues/leagues" , { leagues: allLeagues });
     } catch (error) {
         next(error)
     }
--- services/leagues.service.js
+++ services/leagues.service.js
@@ -1,18 +1,42 @@
 const axios = require("axios");
-const API_KEY = process.env.NEWS_API_KEY;
-
+const API_KEY = process.env.FOOTBALL_API_KEY;
 
 class ApiService {
   constructor() {
     this.api = axios.create({
-      baseURL: "https://v3.football.api-sports.io",
-      headers: { "accept-encoding": "*" },
+      baseURL: `https://v3.football.api-sports.io/leagues`,
+      headers: {
+        "accept-encoding": "*",
+        "x-apisports-key": API_KEY,
+      },
     });
   }
 
   getAllLeagues = () => {
-    return this.api.get('/leagues');
-}
+    return this.api
+      .get(this.baseURL)
+      .then((response) => {
+
+        const selectedLeagues = [];
+
+        response.data.response.forEach(element => {
+          if (
+            element.league.id === 140 ||
+            element.league.id === 135 ||
+            element.league.id === 78 ||
+            element.league.id === 61 ||
+            element.league.id === 39
+            ) {
+              selectedLeagues.push(element);
+              console.log(selectedLeagues);
+          }
+        });
+        return selectedLeagues;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 }
 
 module.exports = ApiService;
