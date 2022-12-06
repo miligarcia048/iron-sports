97549d23ae6e609f6d8f1e07938b72ba91d8be66
Milagros Garcia <miligarcia048@gmail.com>
2022-12-06 21:24:54 +0000

    date format

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ const hbs = require("hbs");
 hbs.registerHelper('ifEquals', function(arg1, arg2, options) {
     return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
 });
+hbs.registerHelper('dateFormat', require('handlebars-dateformat'));
+
 
 const app = express();
 
