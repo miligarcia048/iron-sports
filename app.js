426a9f01e7152e9ddb6129134ff721d19663f2e8
Milagros Garcia <miligarcia048@gmail.com>
2022-12-06 21:10:30 +0000

    table

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ const express = require("express");
 // Handles the handlebars
 // https://www.npmjs.com/package/hbs
 const hbs = require("hbs");
-hbs.registerHelper('paginate', require('handlebars-paginate'));
+hbs.registerHelper('ifEquals', function(arg1, arg2, options) {
+    return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+});
 
 const app = express();
 
