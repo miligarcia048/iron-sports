6d3d5474894ddbba54bbc7dd5397bbb153f87772
Milagros Garcia <miligarcia048@gmail.com>
2022-12-13 19:43:22 +0000

    home

--- routes/news.routes.js
+++ routes/news.routes.js
@@ -1,18 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const ApiService = require('../services/news.service');
+const ApiService = require("../services/news.service");
 const apiService = new ApiService();
 
 router.get("/news", async (req, res, next) => {
-    try {
-      const allNews = await apiService.getAllNews();
-      console.log(allNews)
-      res.render("news/news", { news: allNews });
-    } catch (error) {
-      next(error);
-    }
+  try {
+    const allNews = await apiService.getAllNews();
+    console.log(allNews);
+    res.render("news/news", { news: allNews });
+  } catch (error) {
+    next(error);
+  }
 });
-
 
 module.exports = router;
