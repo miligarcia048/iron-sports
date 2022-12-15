1446555454ea135dae37e6880dcbeb1a9f7943c4
Milagros Garcia <miligarcia048@gmail.com>
2022-12-15 21:34:48 +0000

    styles

--- app.js
+++ app.js
@@ -12,18 +12,25 @@ const express = require("express");
 // Handles the handlebars
 // https://www.npmjs.com/package/hbs
 const hbs = require("hbs");
-hbs.registerHelper('ifEquals', function(arg1, arg2, options) {
-    return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
-});
-hbs.registerHelper('dateFormat', require('handlebars-dateformat'));
 
+hbs.registerHelper("ifEquals", function (arg1, arg2, options) {
+  return arg1 == arg2 ? options.fn(this) : options.inverse(this);
+});
+hbs.registerHelper("dateFormat", require("handlebars-dateformat"));
+
+hbs.registerHelper("length", function (v1, v2, options) {
+  if (v1.length > v2) {
+    return options.fn(this);
+  }
+  return options.inverse(this);
+});
 
 const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
-require("./config/session.config")(app)
-require('./config/passport.config');
+require("./config/session.config")(app);
+require("./config/passport.config");
 
 // default value for title local
 const capitalize = require("./utils/capitalize");
@@ -36,19 +43,15 @@ const indexRoutes = require("./routes/index.routes");
 const leagueRoutes = require("./routes/leagues.routes");
 const newsRoutes = require("./routes/news.routes");
 const teamRoutes = require("./routes/team.routes");
-const authRoutes = require('./routes/auth.routes');
-
-
+const authRoutes = require("./routes/auth.routes");
 
 app.use("/", indexRoutes);
 app.use("/", leagueRoutes);
 app.use("/", newsRoutes);
 app.use("/", teamRoutes);
-app.use('/', authRoutes);
-
+app.use("/", authRoutes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
-
 
 module.exports = app;
